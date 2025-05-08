e0a0f8313c0ad6206f718c6b312442a630ca48ab
karim said <karimsaid214@gmail.com>
2025-05-08 20:49:10 +0300

    Dashboard with chadCN and Next js

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { LogOut, Moon, Settings, User } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function Navbar() {
+  return (
+    <nav className=" p-4 flex items-center justify-between">
+      {/* left side */}
+      <Button>collapse button</Button>
+      <div className="flex items-center justify-between gap-8">
+        <Moon />
+        <Link href=""> Dashboard</Link>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="h-[1.2rem] w-[1.2rem]  mr-2" /> Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="h-[1.2rem] w-[1.2rem]  mr-2"/> Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem]  mr-2"/> Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+}
